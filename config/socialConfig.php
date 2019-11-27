b8851185a787c473a0eab52a505f196f2ad80ecd
Mohammed Minuddin Peal <moinuddin7@gmail.com>
2019-11-27 17:29:55 +0600

    Update social config file

--- config/socialConfig.php
+++ config/socialConfig.php
@@ -3,17 +3,17 @@ return [
     'facebook' => [
         'app_id' => env('FACEBOOK_APP_ID'), // Replace {app_id} with your app id
         'app_secret' => env('FACEBOOK_APP_SECRET'),
-        'default_graph_version' => env('FACEBOOK_GRAPH_VERSION'),//'v2.2',
-        'call_back_url' => env('FACEBOOK_CALLBACK_URL'),
+        'default_graph_version' => 'v2.6',
+        'call_back_url' => 'http://localhost:8000/facebook-success',
         'scope' => [
             'email'
         ]
     ],
     'google' => [
-        'application_name' => env('GOOLE_CONSOLE_APPLICATION_NAME'),
+        'application_name' => 'your-application-name',
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect_url' => env('GOOGLE_REDIRECT_URL'),
+        'redirect_url' => 'http://localhost:8000/google-success',
         'scope' => [
             'https://www.googleapis.com/auth/plus.me',
             'https://www.googleapis.com/auth/userinfo.email',
@@ -22,12 +22,11 @@ return [
     ],
 
     'github' => [
-        'app_id' => env('GITHUB_APP_ID'),
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'authorize_url' => 'https://github.com/login/oauth/authorize',
         'token_url' => 'https://github.com/login/oauth/access_token',
         'api_url_base' => 'https://api.github.com/',
-        'call_back_url' => env('GITHUB_CALLBACK_URL')
+        'call_back_url' => 'http://localhost:8000/gitloginsuccess'
     ],
 ];
